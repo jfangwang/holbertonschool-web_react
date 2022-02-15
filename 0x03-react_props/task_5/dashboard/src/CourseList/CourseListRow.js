cb6d2b39c88bb03cd7e3ffe6ccb5213174eef06f
Jonny Wang <qbs1864@gmail.com>
2022-02-15 19:52:32 +0000

    all props have default values

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseListRow.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseListRow.js
@@ -19,6 +19,7 @@ export default function CourseListRow({isHeader, textFirstCell, textSecondCell})
 }
 CourseListRow.defaultProps = {
   isHeader: false,
+  textFirstCell: null,
   textSecondCell: null 
 }
 CourseListRow.propTypes = {
