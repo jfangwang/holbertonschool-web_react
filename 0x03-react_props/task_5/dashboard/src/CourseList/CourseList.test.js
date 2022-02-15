ebf01f11f9a49742b63df61aa5c71091d7771d94
Jonny Wang <qbs1864@gmail.com>
2022-02-15 19:47:33 +0000

    finised unit testing

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -2,13 +2,24 @@ import React from "react";
 import { shallow } from "enzyme";
 import CourseList from "./CourseList";
 
+const courses = [
+    {id: 3, name: "Science", credit: 15},
+    {id: 1, name: "History", credit: 4},
+    {id: 2, name: "Writing", credit: 80}
+  ]
+
 describe("CourseList Test Suite", () => {
-    it("Renders CourseList without crashing", () => {
-        const wrapper = shallow(<CourseList/>);
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<CourseList listCourses={courses}/>);
+    })
+    it("Renders correctly when passing in empty array or undefined", () => {
+        wrapper = shallow(<CourseList/>);
+        const wrapper2 = shallow(<CourseList listCourses={[]}/>);
         expect(wrapper.exists());
+        expect(wrapper2.exists());
     });
     it("Renders 5 rows correctly", () => {
-        const wrapper = shallow(<CourseList/>);
         expect(wrapper.find('CourseListRow')).toHaveLength(5);
         expect(wrapper.find('CourseListRow').at(0).prop('textFirstCell', 'Available courses'));
         expect(wrapper.find('CourseListRow').at(0).prop('isHeader', 'true'));
@@ -24,5 +35,17 @@ describe("CourseList Test Suite", () => {
         expect(wrapper.find('CourseListRow').at(4).prop('isHeader', 'false'));
         expect(wrapper.find('CourseListRow').at(4).prop('textFirstCell', 'React'));
         expect(wrapper.find('CourseListRow').at(4).prop('textSecondCell', '40'));
+    });
+    it("Outputs the correct items", () => {
+        expect(wrapper.find('CourseListRow')).toHaveLength(5);
+        expect(wrapper.find('CourseListRow').at(0).prop('id', 3));
+        expect(wrapper.find('CourseListRow').at(0).prop('textFirstCell', 'Science'));
+        expect(wrapper.find('CourseListRow').at(0).prop('textSecondCell', 15));
+        expect(wrapper.find('CourseListRow').at(1).prop('id', 1));
+        expect(wrapper.find('CourseListRow').at(1).prop('textFirstCell', 'History'));
+        expect(wrapper.find('CourseListRow').at(1).prop('textSecondCell', 4));
+        expect(wrapper.find('CourseListRow').at(2).prop('id', 2));
+        expect(wrapper.find('CourseListRow').at(2).prop('textFirstCell', 'Writing'));
+        expect(wrapper.find('CourseListRow').at(2).prop('textSecondCell', 80));
     });
 });
--- 0x03-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -2,22 +2,31 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Notifications from './Notifications'
 import NotificationItem from './NotificationItem'
+import { getLatestNotification } from '../utils/utils';
+
+const notificationsList = [
+  {id: 1, type: "default", value: "Testing..."},
+  {id: 2, type: "urgent", value: "Test 2"},
+  {id: 3, type: "urgent", html: {__html: getLatestNotification()}, value:"Test 3"}
+]
 
 describe('Notifications Test Suite', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<Notifications displayDrawer={true}/>);
+    })
     it('Test Notifications renders without crashing', () => {
       const wrapper = shallow(<Notifications/>);
       expect(wrapper.exists()).toEqual(true);
     });
     it('Test renders three list items', () => {
-        const wrapper = shallow(<Notifications displayDrawer={true}/>);
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={notificationsList}/>);
         expect(wrapper.find("ul").children()).toHaveLength(3);
       });
     it('Test renders the text Here is the list of notifications', () => {
-        const wrapper = shallow(<Notifications displayDrawer={true}/>);
         expect(wrapper.find(".Notifications p").text()).toEqual("Here is the list of notifications");
     });
     it('Renders NotificationItem Component when True', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true}/>);
       expect(wrapper.containsMatchingElement(<NotificationItem />)).toEqual(true);
     })
     it('div.menuItem is being displayed when displayDrawer=False', () => {
@@ -29,11 +38,25 @@ describe('Notifications Test Suite', () => {
       expect(wrapper.find('div.Notifications')).toHaveLength(0);
     })
     it('div.menuItem is not being displayed when displayDrawer=True', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true}/>);
       expect(wrapper.find('div.menuItem')).toHaveLength(1);
     })
     it('div.Notifications is being displayed when displayDrawer=True', () => {
-      const wrapper = shallow(<Notifications displayDrawer={true}/>);
       expect(wrapper.find('div.Notifications')).toHaveLength(1);
+    })
+    it('Renders when passing in empty array or undefined listNotifications', () => {
+      const wrapper2 = shallow(<Notifications displayDrawer={true} listNotifications={[]}/>);
+      expect(wrapper.exists()).toEqual(true);
+      expect(wrapper2.exists()).toEqual(true);
+    })
+    it('Renders when passing in a list of notifications with corrent num of items', () => {
+      const wrapper2 = shallow(<Notifications displayDrawer={true} listNotifications={notificationsList}/>);
+      expect(wrapper2.find('div.Notifications ul NotificationItem')).toHaveLength(3);
+      expect(wrapper2.find('div.Notifications ul NotificationItem').at(0).prop('value')).toEqual("Testing...");
+      expect(wrapper2.find('div.Notifications ul NotificationItem').at(1).prop('type')).toEqual("urgent");
+    })
+    it('Correct message when listNotifications is empty', () => {
+      const wrapper2 = shallow(<Notifications displayDrawer={true} listNotifications={[]}/>);
+      expect(wrapper.find('div.Notifications ul NotificationItem').prop('value')).toEqual("No new notification for now");
+      expect(wrapper2.find('div.Notifications ul NotificationItem').prop('value')).toEqual("No new notification for now");
     })
   });
