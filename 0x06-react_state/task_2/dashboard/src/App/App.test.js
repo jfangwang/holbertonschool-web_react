e70496388d253b83bf4387507371799df530ad34
jfangwang <qbs1864@gmail.com>
2022-03-01 13:26:10 -0500

    task 2 testing done

--- 0x06-react_state/task_2/dashboard/src/App/App.test.js
+++ 0x06-react_state/task_2/dashboard/src/App/App.test.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
 import CourseList from '../CourseList/CourseList';
 import Login from '../Login/Login';
@@ -11,63 +11,87 @@ import { StyleSheetTestUtils } from 'aphrodite';
 
 
 describe('App Test Suite', () => {
-    beforeEach(() => {
-      StyleSheetTestUtils.suppressStyleInjection();
-    });
-    afterEach(() => {
-      StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-    });
-    it('App renders without crashing', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.exists());
-    });
-    // it('Verify that App renders a div with the class App-body', () => {
-    //   const wrapper = shallow(<App/>);
-    //   expect(wrapper.find('div.App-body')).toHaveLength(1);
-    // });
-    it('Contains Notifications Component', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.find('Notifications')).toHaveLength(1);
-    });
-    it('Contains Header Component', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.find('Header')).toHaveLength(1);
-    });
-    it('Contains Login Component', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.find('Login')).toHaveLength(1);
-    });
-    it('Contains Footer Component', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.find('Footer')).toHaveLength(1);
-    });
-    it('CourseList is not displayed', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.find(CourseList)).toHaveLength(0);
-    });
-    it('CourseList is not displayed', () => {
-      const wrapper = shallow(<App isLoggedIn={true}/>);
-      expect(wrapper.find(Login)).toHaveLength(1);
-      expect(wrapper.find(CourseList)).toHaveLength(0);
-    });
-    it('Login true', () => {
-      const wrapper = shallow(<App isLoggedIn={true}/>);
-      expect(wrapper.find('Login')).toHaveLength(1);
-      expect(wrapper.find('CourseList')).toHaveLength(0);
-    })
-    it('logout works', () => {
-      const func = jest.fn(() => undefined);
-      const wrapper = shallow(<App logOut={func}/>);
-      const spy = jest.spyOn(global, 'alert')
-      expect(spy);
-      expect(func);
-    })
-    it('displayDrawer', () => {
-      const wrapper = shallow(<App/>);
-      expect(wrapper.state('displayDrawer')).toEqual(false);
-      wrapper.instance().handleDisplayDrawer();
-      expect(wrapper.state('displayDrawer')).toEqual(true);
-      wrapper.instance().handleHideDrawer();
-      expect(wrapper.state('displayDrawer')).toEqual(false);
-    })
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
   });
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  it('App renders without crashing', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.exists());
+  });
+  // it('Verify that App renders a div with the class App-body', () => {
+  //   const wrapper = shallow(<App/>);
+  //   expect(wrapper.find('div.App-body')).toHaveLength(1);
+  // });
+  it('Contains Notifications Component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('Notifications')).toHaveLength(1);
+  });
+  it('Contains Header Component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('Header')).toHaveLength(1);
+  });
+  it('Contains Login Component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('Login')).toHaveLength(1);
+  });
+  it('Contains Footer Component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('Footer')).toHaveLength(1);
+  });
+  it('CourseList is not displayed', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
+  it('CourseList is not displayed', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find(Login)).toHaveLength(1);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
+  it('Login true', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find('Login')).toHaveLength(1);
+    expect(wrapper.find('CourseList')).toHaveLength(0);
+  })
+  it('displayDrawer', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.state('displayDrawer')).toEqual(false);
+    wrapper.instance().handleDisplayDrawer();
+    expect(wrapper.state('displayDrawer')).toEqual(true);
+    wrapper.instance().handleHideDrawer();
+    expect(wrapper.state('displayDrawer')).toEqual(false);
+  })
+});
+describe('Check states for isLoggedIn', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    const wrapper = shallow(<App />);
+  });
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  it('Login func updates the states correctly', () => {
+    const wrapper = shallow(<App/>);
+    wrapper.setState({
+      user: {
+        email: 'willy',
+        password: 'free',
+        isLoggedIn: true
+      }
+    })
+    expect(wrapper.exists());
+    expect(wrapper.state().user.email).toBe('willy');
+    expect(wrapper.state().user.password).toBe('free');
+    expect(wrapper.state().user.isLoggedIn).toBe(true);
+  });
+  it('LogOut func updates the states correctly', () => {
+    const wrapper = mount(<App/>);
+    expect(wrapper.exists());
+    wrapper.state().logOut();
+    expect(wrapper.state().user.email).toBe('');
+    expect(wrapper.state().user.password).toBe('');
+    expect(wrapper.state().user.isLoggedIn).toBe(false);
+  });
+});
