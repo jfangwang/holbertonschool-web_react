dc6d9d2d1e78d6384f6051951d5360b460bc2da9
jfangwang <qbs1864@gmail.com>
2022-03-01 11:25:04 -0500

    logout works

--- 0x06-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x06-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/Holberton_Logo.jpg';
 import { StyleSheet, css } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
 const styles = StyleSheet.create({
   AppLogo: {
@@ -15,13 +16,35 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     display: 'flex'
   },
+  logout: {
+    cursor: 'pointer',
+  },
+  welcome: {
+    margin: '1rem 0 0 0',
+  },
 });
 
-export default function Header() {
-  return (
-    <div className={css(styles.AppHeader)}>
-      <img src={logo} className={css(styles.AppLogo)} alt="holberton logo" />
-      <h1>School Dashboard</h1>
-    </div>
-  );
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let value = this.context;
+    return (
+      <>
+        <div className={css(styles.AppHeader)}>
+          <img src={logo} className={css(styles.AppLogo)} alt="holberton logo" />
+          <h1>School Dashboard</h1>
+        </div>
+        { value.user.isLoggedIn && (
+          <div className={css(styles.welcome)} id='logoutSection'>
+            welcome <b>{value.user.email}</b> <a className={css(styles.logout)} onClick={value.logOut}>(logout)</a>
+          </div>
+        )}
+      </>
+    );
+  }
 }
+
+Header.contextType = AppContext;
+export default Header;
