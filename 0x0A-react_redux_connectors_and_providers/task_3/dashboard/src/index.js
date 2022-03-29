12c4ee7d1afb804257b31d6f7e1c24a04cfd3953
jfangwang <qbs1864@gmail.com>
2022-03-29 11:06:42 -0400

    3 done

--- 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import { uiReducer } from './reducers/uiReducer';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from "redux-devtools-extension";
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 
-const store = createStore(uiReducer, applyMiddleware(thunk));
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(uiReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
